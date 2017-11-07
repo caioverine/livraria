f3d452d94b093dd1b3b39554f7d52aee2f34c48d
caio.cvac <caio.cvac@dpf.gov.br>
2017-11-07 08:09:27 -0200

    Aula 01

--- src/br/com/caelum/livraria/bean/LivroBean.java
+++ src/br/com/caelum/livraria/bean/LivroBean.java
@@ -3,6 +3,7 @@ package br.com.caelum.livraria.bean;
 import java.util.List;
 
 import javax.enterprise.inject.Model;
+import javax.inject.Inject;
 
 import br.com.caelum.livraria.dao.AutorDao;
 import br.com.caelum.livraria.dao.LivroDao;
@@ -14,8 +15,9 @@ public class LivroBean {
 	
 	private Livro livro = new Livro();
 	private Integer autorId;
-	
+	@Inject
 	private LivroDao livroDao = new LivroDao();
+	@Inject
 	private AutorDao autorDao = new AutorDao();
 
 	public void cadastra() {
