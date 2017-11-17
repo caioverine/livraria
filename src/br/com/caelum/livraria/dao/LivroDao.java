42d3e479f28cbe95b0a38619b04a76cd2221072b
caio.cvac <caio.cvac@dpf.gov.br>
2017-11-17 08:35:24 -0200

    Aula 07

--- src/br/com/caelum/livraria/dao/LivroDao.java
+++ src/br/com/caelum/livraria/dao/LivroDao.java
@@ -3,11 +3,10 @@ package br.com.caelum.livraria.dao;
 import java.util.List;
 
 import javax.ejb.Stateless;
-import javax.interceptor.Interceptors;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 
-import br.com.caelum.livraria.interceptador.LogInterceptador;
 import br.com.caelum.livraria.modelo.Livro;
 
 @Stateless
@@ -26,5 +25,15 @@ public class LivroDao {
 		
 		return manager.createQuery("select l from Livro l", Livro.class).getResultList();
 	}
+	
+	public List<Livro> livrosPeloNome(String nome) {
+
+        TypedQuery<Livro> query = this.manager.createQuery(
+                "select l from Livro l " + 
+                "where l.titulo like :pTitulo", Livro.class);
+        query.setParameter("pTitulo", "%" + nome + "%");
+
+        return query.getResultList();
+    }
 	
 }
